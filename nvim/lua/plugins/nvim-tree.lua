f69c4a5297ddeac3cbea41bdab260bd5a6eacf8c
itomise <itomise.mizumi@gmail.com>
2025-03-30 15:00:11 +0900

    fix: nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,26 +1,30 @@
-local function collapse_or_parent()
-  local api = require("nvim-tree.api")
-  local node = api.tree.get_node_under_cursor()
-
-  if node.nodes ~= nil then
-    api.node.navigate.parent_close()
-  else
-    api.node.navigate.parent()
-  end
-end
-
-local function edit_or_open()
-  local api = require("nvim-tree.api")
-  -- expand or collapse folder
-  api.node.open.edit()
-end
-
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = true,
-  keys = {
-    { mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>" },
-    { mode = "n", "h", collapse_or_parent },
-    { mode = "n", "l", edit_or_open },
-  },
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		require("nvim-tree").setup({
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				local function collapse_or_parent()
+					local node = api.tree.get_node_under_cursor()
+					if node.nodes then
+						api.node.navigate.parent_close()
+					else
+						api.node.navigate.parent()
+					end
+				end
+
+				local function edit_or_open()
+					api.node.open.edit()
+				end
+
+				local opts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "h", collapse_or_parent, opts)
+				vim.keymap.set("n", "l", edit_or_open, opts)
+			end,
+		})
+	end,
+	keys = {
+		{ mode = "n", "<C-n>", "<cmd>NvimTreeToggle<CR>" },
+	},
 }
