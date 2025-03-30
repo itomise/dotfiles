5b05a041e40ecdf2bd97774febe163c9bd881f1b
itomise <itomise.mizumi@gmail.com>
2025-03-30 15:50:22 +0900

    feat: change formatter

---
+++ nvim/lua/plugins/conform.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, lsp_fallback = true })
+        end,
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        typescript = { "prettier" },
+        javascript = { "prettier" },
+        css = { "prettier" },
+        markdown = { "prettier" },
+      },
+      format_on_save = {
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+  },
+}
