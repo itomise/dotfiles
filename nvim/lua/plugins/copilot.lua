39831a42ef68f3dfcc846d472ef49d025b5a600a
itomise <itomise.mizumi@gmail.com>
2025-03-30 22:19:57 +0900

    fix: change copilot accept key

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -19,14 +19,15 @@ return {
         suggestion = {
           enabled = true,
           auto_trigger = true,
+          hide_during_completion = true,
           debounce = 75,
           keymap = {
-            accept = "<M-l>",
-            accept_word = false,
+            accept = "<C-g><C-l>",
+            accept_word = "<C-g><C-w>",
             accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
+            next = "<C-g><C-n>",
+            prev = "<C-g><C-p>",
+            dismiss = "<C-g><BS>",
           },
         },
         filetypes = {
@@ -39,5 +40,5 @@ return {
         },
       })
     end,
-  }
+  },
 }
