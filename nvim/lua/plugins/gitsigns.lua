f0bfb85f5a223374c675fa742a7d3379100857c9
itomise <itomise.mizumi@gmail.com>
2025-03-31 00:38:48 +0900

    feat: add gitsign

---
+++ nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = "│" },
+          change = { text = "│" },
+          delete = { text = "_" },
+          topdelete = { text = "‾" },
+          changedelete = { text = "~" },
+          untracked = { text = "┆" },
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          -- Actions
+          map("n", "<leader>gb", gs.toggle_current_line_blame, { desc = "Toggle blame" })
+          map("n", "<leader>gB", function()
+            gs.blame_line({ full = true })
+          end, { desc = "Blame line" })
+          map("n", "<leader>gp", gs.preview_hunk, { desc = "Preview hunk" })
+          map("n", "<leader>gd", gs.diffthis, { desc = "Diff this" })
+          map("n", "<leader>gD", function()
+            gs.diffthis("~")
+          end, { desc = "Diff this ~" })
+        end,
+        current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+          delay = 100,
+          ignore_whitespace = false,
+        },
+        current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+      })
+    end,
+  },
+}
