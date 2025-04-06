01df8652c7becd09fa7cf37db058c347416a6920
itomise <itomise.mizumi@gmail.com>
2025-04-07 00:59:06 +0900

    chore

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,7 +11,7 @@ return {
       },
     },
     keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>",            desc = "Find files" },
+      { "<leader>ff", "<cmd>Telescope git_files<cr>",             desc = "Find files (git_files)" },
       { "<leader>fg", "<cmd>Telescope live_grep<cr>",             desc = "Live grep" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>",               desc = "Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>",             desc = "Help tags" },
