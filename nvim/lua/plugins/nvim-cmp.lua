10bf7bab9ffdcad3fd4d8114e527635fceee6a1d
itomise <itomise.mizumi@gmail.com>
2025-04-06 20:07:43 +0900

    feat: cmp

---
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,54 @@
+return {
+  -- 補完エンジン本体
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp", -- LSP用の補完ソース
+      "hrsh7th/cmp-buffer", -- バッファ用の補完ソース
+      "hrsh7th/cmp-path", -- パス補完ソース
+      "hrsh7th/cmp-cmdline", -- コマンドライン用補完
+      "L3MON4D3/LuaSnip", -- スニペットエンジン
+      "saadparwaiz1/cmp_luasnip", -- スニペット用の補完ソース
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" },
+        }),
+      })
+
+      -- コマンドライン補完の設定 (任意)
+      cmp.setup.cmdline({ "/", "?" }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = { { name = "buffer" } },
+      })
+
+      cmp.setup.cmdline(":", {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = "path" },
+        }, {
+          { name = "cmdline" },
+        }),
+      })
+    end,
+  },
+}
