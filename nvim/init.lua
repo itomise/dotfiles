09183ba0f8d214cea9846bb19d97a53b3f9aea2e
itomise <itomise.mizumi@gmail.com>
2025-04-13 15:22:01 +0900

    feat: apply vscode settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,54 @@
+-- インデント設定
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+-- シンタックスを有効化
+vim.cmd("syntax enable")
+
+-- カラースキームを設定
+-- vim.cmd('colorscheme tokyonight')  -- または他のカラースキーム
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- 行番号を表示
+vim.opt.number = true
+
+-- クリップボードを有効化 (schedule で遅延実行)
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+vim.opt.confirm = true
+vim.opt.swapfile = false
+
+-- キーマップ設定を読み込む
+require("keymaps")
+
+vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#553311" })
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 200 })
+  end,
+})
+
 if vim.g.vscode then
   vim.opt.clipboard = "unnamedplus"
 else
-  -- インデント設定
-  vim.opt.expandtab = true -- タブをスペースに変換
-  vim.opt.shiftwidth = 2 -- インデント幅を2に設定
-  vim.opt.tabstop = 2 -- タブ文字の表示幅を2に設定
-  vim.opt.softtabstop = 2 -- タブキーを押したときのスペースの数を2に設定
-
-  -- シンタックスを有効化
-  vim.cmd("syntax enable")
-
-  -- カラースキームを設定
-  -- vim.cmd('colorscheme tokyonight')  -- または他のカラースキーム
-
-  vim.g.mapleader = " "
-  vim.g.maplocalleader = "\\"
-
-  -- 行番号を表示
-  vim.opt.number = true
-
-  -- クリップボードを有効化 (schedule で遅延実行)
-  vim.schedule(function()
-    vim.opt.clipboard = "unnamedplus"
-  end)
-
-  vim.opt.breakindent = true
-  vim.opt.undofile = true
-  vim.opt.ignorecase = true
-  vim.opt.smartcase = true
-  vim.opt.signcolumn = "yes"
-  vim.opt.updatetime = 250
-  vim.opt.timeoutlen = 300
-  vim.opt.splitright = true
-  vim.opt.splitbelow = true
-  vim.opt.inccommand = "split"
-  vim.opt.cursorline = true
-  vim.opt.scrolloff = 10
-  vim.opt.confirm = true
-  vim.opt.swapfile = false
   vim.opt.mouse = "a"
-
-  -- キーマップ設定を読み込む
-  require("keymaps")
 
   -- plugin
   require("config.lazy")
--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -4,51 +4,16 @@ vim.keymap.set("i", "っj", "<ESC>", { desc = "Exit insert mode with jj" })
 vim.keymap.set("i", "っっj", "<ESC>", { desc = "Exit insert mode with jj" })
 vim.keymap.set("i", "っっっj", "<ESC>", { desc = "Exit insert mode with jj" })
 
--- ノーマルモードで <Esc> を押すと検索のハイライトをクリア
--- 詳細は `:help hlsearch` を参照
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- 別ウィンドウで編集された内容を自動で反映する
-vim.opt.autoread = true
-
--- ファイル変更を自動検出して更新を反映するための設定
--- 定期的にバッファの変更を確認して更新
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
-  pattern = "*",
-  callback = function()
-    if vim.fn.mode() ~= "c" then
-      vim.cmd("checktime")
-    end
-  end,
-})
-
--- タイマーイベントを設定して定期的に checktime を実行
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.fn.timer_start(1000, function()
-      vim.cmd("checktime")
-      return 1000             -- 1秒ごとに実行を継続
-    end, { ["repeat"] = -1 }) -- 無限に繰り返す
-  end,
-})
-
 -- 診断キーマップ
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
-
--- NvimTree focus on current file
-vim.keymap.set("n", "<C-g>", function()
-  local api = require("nvim-tree.api")
-  api.tree.find_file({ open = true, focus = true })
-end, { desc = "Focus current file in nvim-tree" })
 
 vim.keymap.set("n", "q", "<Nop>", { desc = "Disable q key in normal mode" })
 
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit Vim" })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+
+-- ノーマルモードで <Esc> を押すと検索のハイライトをクリア
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- window (vertical)
 vim.keymap.set("n", "=", function()
@@ -66,4 +31,49 @@ vim.keymap.set("n", "_", function()
   vim.cmd("resize +5")
 end, { desc = "resize horizontal +5" })
 
-vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode with Escape" })
+if not vim.g.vscode then
+  -- window move
+  vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+  vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+  vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+  vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+
+  vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode with Escape" })
+
+  -- 別ウィンドウで編集された内容を自動で反映する
+  vim.opt.autoread = true
+
+  -- ファイル変更を自動検出して更新を反映するための設定
+  -- 定期的にバッファの変更を確認して更新
+  vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
+    pattern = "*",
+    callback = function()
+      if vim.fn.mode() ~= "c" then
+        vim.cmd("checktime")
+      end
+    end,
+  })
+
+  -- タイマーイベントを設定して定期的に checktime を実行
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      vim.fn.timer_start(1000, function()
+        vim.cmd("checktime")
+        return 1000             -- 1秒ごとに実行を継続
+      end, { ["repeat"] = -1 }) -- 無限に繰り返す
+    end,
+  })
+
+  -- NvimTree focus on current file
+  vim.keymap.set("n", "<C-g>", function()
+    local api = require("nvim-tree.api")
+    api.tree.find_file({ open = true, focus = true })
+  end, { desc = "Focus current file in nvim-tree" })
+else
+  -- tab move
+  vim.keymap.set("n", "<C-h>", "<Cmd>call VSCodeNotify('workbench.action.previousEditor')<CR>")
+  vim.keymap.set("n", "<C-l>", "<Cmd>call VSCodeNotify('workbench.action.nextEditor')<CR>")
+
+  -- comment out
+  vim.keymap.set({ "n", "v" }, "gc", "<cmd>call VSCodeNotify('editor.action.commentLine')<CR>")
+end
