b0517e632468b367950320038a4868f30664c370
itomise <itomise.mizumi@gmail.com>
2025-04-26 21:28:29 +0900

    feat: perf

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -18,8 +18,12 @@ return {
         highlight = {
           enable = true,
           disable = function(lang, buf)
-            local MAX_FILESIZE = 2 * 1024 * 1024
+            -- Disable Treesitter for markdown files to improve performance
+            if lang == "markdown" then
+              return true
+            end
 
+            local MAX_FILESIZE = 2 * 1024 * 1024
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > MAX_FILESIZE then
               return true
