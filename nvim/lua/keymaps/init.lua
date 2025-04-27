7858d0ed86fed5cca29df05faff10cc97fc53f6a
itomise <itomise.mizumi@gmail.com>
2025-04-27 11:36:47 +0900

    fix: disable custom paste for simple clipboard paste

--- nvim/lua/keymaps/init.lua
+++ nvim/lua/keymaps/init.lua
@@ -16,9 +16,9 @@ vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("v", "p", '"0p', { silent = true })
-vim.keymap.set("s", "p", '"0p', { silent = true })
-
+-- vim.keymap.set("v", "p", '"0p', { silent = true })
+-- vim.keymap.set("s", "p", '"0p', { silent = true })
+--
 vim.keymap.set("n", "<leader>*", "cgn")
 
 vim.keymap.set("n", "gx", "<esc>:URLOpenUnderCursor<cr>")
