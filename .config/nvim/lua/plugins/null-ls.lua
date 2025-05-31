5a4d14a61bbc0c02307acf4b4cb458bdc71deac9
itomise <itomise.mizumi@gmail.com>
2025-05-31 13:24:12 +0900

    fix: deno の project では prettier を使わない

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,8 +1,8 @@
 return {
   "nvimtools/none-ls.nvim",
   dependencies = {
-    "nvim-lua/plenary.nvim", -- none-ls の依存関係
-    "williamboman/mason.nvim", -- Mason と統合するために必要
+    "nvim-lua/plenary.nvim",       -- none-ls の依存関係
+    "williamboman/mason.nvim",     -- Mason と統合するために必要
     "jayp0521/mason-null-ls.nvim", -- Mason と none-ls の統合
   },
   config = function()
@@ -12,10 +12,23 @@ return {
       automatic_installation = true,
     })
 
+    local function is_deno_project()
+      local deno_files = { "deno.json", "deno.jsonc", "deno.lock" }
+      for _, file in ipairs(deno_files) do
+        if vim.fn.filereadable(file) == 1 then
+          return true
+        end
+      end
+      return false
+    end
+
     -- null-ls のソース設定
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.prettier.with({
+          condition = function()
+            return not is_deno_project()
+          end,
           filetypes = { "typescriptreact", "typescript", "json" },
           prefer_local = "node_modules/.bin",
           cwd = function(params)
